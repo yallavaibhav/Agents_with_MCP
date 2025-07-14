f1f6a09423c839dcca356fb87b4035ace717b181
Vaibhav Yalla <39133612+yallavaibhav@users.noreply.github.com>
2025-07-14 07:41:28 -0400

    Update mathserver.py

--- mathserver.py
+++ mathserver.py
@@ -12,20 +12,20 @@ def subtract(a: int, b: int) -> int:
     """Subtract two numbers"""
     return a - b
 
-# @mcp.tool()
-# def multiply(a: int, b: int) -> int:
-#     """Multiply two numbers"""
-#     return a * b
+@mcp.tool()
+def multiply(a: int, b: int) -> int:
+    """Multiply two numbers"""
+    return a * b
 
-# @mcp.tool()
-# def divide(a: int, b: int) -> float:
-#     """Divide two numbers"""
-#     return a / b
+@mcp.tool()
+def divide(a: int, b: int) -> float:
+    """Divide two numbers"""
+    return a / b
 
-# @mcp.tool()
-# def power(a: int, b: int) -> int:
-#     """Raise a number to a power"""
-#     return a ** b  
+@mcp.tool()
+def power(a: int, b: int) -> int:
+    """Raise a number to a power"""
+    return a ** b  
 
 
 ## Use standard i/p and o/p (stdin and stdout) to communicate with the server (rool funciton call)  This will run in cmd. So working locally.
